2cf2f550530c7e88f173b754259fa71a220898a9
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-17 14:12:46 +0400

    Update network.tf

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -1,12 +1,12 @@
-resource "azurerm_virtual_network" "test" {
+resource "azurerm_virtual_network" "flask-webappP3-NET" {
   name                 = "${var.application_type}-${var.resource_type}"
   address_space        = "${var.address_space}"
   location             = "${var.location}"
   resource_group_name  = "${var.resource_group}"
 }
-resource "azurerm_subnet" "test" {
+resource "azurerm_subnet" "flask-webappP3-NET-sub" {
   name                 = "${var.application_type}-${var.resource_type}-sub"
   resource_group_name  = "${var.resource_group}"
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
+  virtual_network_name = "${azurerm_virtual_network.flask-webappP3-NET}"
   address_prefix       = "${var.address_prefix_test}"
 }
