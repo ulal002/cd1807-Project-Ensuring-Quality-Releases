5faa9f393ccae8d85fa0e52f2150669036f5e210
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-17 15:59:28 +0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
   features {}
+  skip_provider_registration = true
 }
 terraform {
   backend "azurerm" {
