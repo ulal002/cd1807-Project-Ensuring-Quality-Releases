9504c57856033c90d1132b479be5d63e57f79244
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 15:07:35 +0400

    Update input.tf

--- terraform/modules/appservice/input.tf
+++ terraform/modules/appservice/input.tf
@@ -4,5 +4,6 @@ variable "location" {}
 variable "application_type" {}
 variable "resource_type" {
   type    = string
+  description = "Name of the Azure resource group"
 }
 variable "resource_group_name" {}
