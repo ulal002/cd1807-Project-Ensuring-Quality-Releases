d460c99dde038b83bcf3118fbd016a1adc3e2b43
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 14:04:31 +0400

    Update input.tf

--- terraform/modules/appservice/input.tf
+++ terraform/modules/appservice/input.tf
@@ -3,4 +3,7 @@ variable "location" {}
 
 variable "application_type" {}
 variable "resource_type" {}
-variable "resource_group_name" {}
+variable "resource_group_name" {
+  type = string
+  description = "The name of the resource group for the app service"
+}
