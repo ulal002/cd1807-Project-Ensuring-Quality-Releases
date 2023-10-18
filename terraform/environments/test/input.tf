eb634e57d08e156185076508d93288c7d9be1835
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 12:03:20 +0400

    Update input.tf

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -6,7 +6,7 @@ variable "tenant_id" {}
 
 # Resource Group/Location
 variable "location" {}
-variable "resource_group" {}
+variable "resource_group_name" {}
 variable "application_type" {}
 
 # Network
