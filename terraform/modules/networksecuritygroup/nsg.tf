8401ed7fbb27155d25609d42f602c49e88914cd4
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 15:21:44 +0400

    Update nsg.tf

--- terraform/modules/networksecuritygroup/nsg.tf
+++ terraform/modules/networksecuritygroup/nsg.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
-  resource_group_name = var.resource_group
+  resource_group_name = var.resource_group_name
 
   security_rule {
     name                       = "${var.application_type}-${var.resource_type}-5000"
