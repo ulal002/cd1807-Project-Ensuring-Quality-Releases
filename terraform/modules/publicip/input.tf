83443ec495b4bf35d52ff39633a743eaf493637b
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 15:17:32 +0400

    Update input.tf

--- terraform/modules/publicip/input.tf
+++ terraform/modules/publicip/input.tf
@@ -1,6 +1,9 @@
 # Resource Group/Location
 variable "location" {}
-variable "resource_group" {}
+variable "resource_group_name" {
+  type    = string
+  description = "Name of the Azure resource group"
+}
 
 variable "application_type" {}
 variable "resource_type" {}
