3e6425d2cc7d2463dbf8f7c83ad7593d1c7e3af4
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 11:32:33 +0400

    Update rg.tf

--- terraform/modules/resource_group/rg.tf
+++ terraform/modules/resource_group/rg.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "Udacity-Urvesh" {
-  name     = var.resource_group
+resource "azurerm_resource_group_name" "Udacity-Urvesh" {
+  name     = var.resource_group_name
   location = var.location
 }
