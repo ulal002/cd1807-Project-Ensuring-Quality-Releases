be6310efec2370567686822117287199ea97a16d
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 15:17:51 +0400

    Update input.tf

--- terraform/modules/resource_group/input.tf
+++ terraform/modules/resource_group/input.tf
@@ -1,3 +1,6 @@
 # Resource Group
 variable location {}
-variable "resource_group_name" {}
+variable "resource_group_name" {
+  type    = string
+  description = "Name of the Azure resource group"
+}
