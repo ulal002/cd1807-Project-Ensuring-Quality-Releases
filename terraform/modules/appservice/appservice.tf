d01ece45ae3ff82b3c1536cb4cfacbe1a07c6859
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 11:30:18 +0400

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,7 +1,7 @@
 resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
-  resource_group_name = var.resource_group
+  resource_group_name = var.resource_group_name
   os_type             = "Linux"
   sku_name            = "F1"
 }
@@ -9,7 +9,7 @@ resource "azurerm_service_plan" "test" {
 resource "azurerm_linux_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
-  resource_group_name = var.resource_group
+  resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
