7f1a95142ed31c5f7f6c4c274d4744c460b6b25c
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-18 15:21:24 +0400

    Update publicip.tf

--- terraform/modules/publicip/publicip.tf
+++ terraform/modules/publicip/publicip.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "test" {
   name                = "${var.application_type}-${var.resource_type}-pubip"
   location            = var.location
-  resource_group_name = var.resource_group
+  resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
 
 }
