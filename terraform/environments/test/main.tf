9ab2f8af69f9d1dfbef5c893d7e7870cd4a71157
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-10-19 11:06:54 +0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -26,7 +26,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group_name       = "${module.resource_group.resource_group_name}"
+  resource_group_name  = module.resource_group.resource_group_name
   address_prefix_test  = "${var.address_prefix_test}"
 }
 
@@ -35,7 +35,7 @@ module "nsg-test" {
   location         = var.location
   application_type = "${var.application_type}"
   resource_type    = "NSG"
-  resource_group_name   = "${module.resource_group.resource_group_name}"
+  resource_group_name   = module.resource_group.resource_group_name
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
 }
@@ -44,12 +44,12 @@ module "appservice" {
   location         = var.location
   application_type = "${var.application_type}"
   resource_type    = "AppService"
-  resource_group_name = "${module.resource_group.resource_group_name}"
+  resource_group_name = module.resource_group.resource_group_name
 }
 module "publicip" {
   source           = "../../modules/publicip"
   location         = var.location
   application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group_name   = "${module.resource_group.resource_group_name}"
+  resource_group_name   = module.resource_group.resource_group_name
 }
