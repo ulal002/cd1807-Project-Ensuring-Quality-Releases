70bf98efcb7ba23a74a39d9d952a10b8d310b83b
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-11-08 16:39:31 +0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -33,6 +33,6 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${module.resource_group.resource_group_name}"
+  resource_group       = module.resource_group.resource_group_name
   address_prefix_test  = "${var.address_prefix_test}"
 }
