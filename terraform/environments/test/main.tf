62d2c4953a54a4f045959a0cee6f6d6c45de3040
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-11-08 16:33:00 +0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -26,12 +26,12 @@ module "resource_group" {
   location             = var.location
 }
 module "network" {
-  source               = "../../modules/network"
-  address_space        = "${var.address_space}"
+  source               = "./modules/network"
+  address_space        = var.address_space
   location             = var.location
-  virtual_network_name = "${var.virtual_network_name}"
-  application_type     = "${var.application_type}"
+  virtual_network_name = var.virtual_network_name
+  application_type     = var.application_type
   resource_type        = "NET"
-  resource_group_name  = module.resource_group.resource_group_name
-  address_prefix_test  = "${var.address_prefix_test}"
+  resource_group       = module.resource_group.resource_group_name
+  address_prefix_test  = var.address_prefix_test
 }
