1e04128c83844cd2c584e7395e0cce3d75389542
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-11-10 10:43:15 +0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -33,5 +33,10 @@ module "network" {
 module "resource_group" {
   source               = "../../modules/resource_group"
   resource_group       = "${var.resource_group}"
+  location             = "${var.location}"
+}
+module "resource_group" {
+  source               = "../../modules/resource_group"
+  resource_group       = "${var.resource_group}"
   location             = var.location
 }
