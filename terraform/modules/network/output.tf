2bdfa7cd41f554046d33f6ccbbcbb9bbc945f691
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-11-14 12:38:22 +0400

    Update output.tf

--- terraform/modules/network/output.tf
+++ terraform/modules/network/output.tf
@@ -1,5 +1,5 @@
 output "rg_name_value" {
-  value = module.resource_group.resource_group_name
+  value = module.network.resource_group_name
 }
 output "subnet_id_test" {
   value = "${azurerm_subnet.test.id}"
