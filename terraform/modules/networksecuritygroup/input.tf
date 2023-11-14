b9a9f36e1f14f736bcc62cf9d69c446fc487b774
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-11-14 10:52:11 +0400

    Update input.tf

--- terraform/modules/networksecuritygroup/input.tf
+++ terraform/modules/networksecuritygroup/input.tf
@@ -2,9 +2,6 @@
 variable "location" {}
 variable "application_type" {}
 variable "resource_type" {}
-variable "resource_group_name" {
-  type    = string
-  description = "Name of the Azure resource group"
-}
+variable resource_group_name {}
 variable "subnet_id" {}
 variable "address_prefix_test" {}
