b7ec45d2582f90f85acea0bffff5e6eba335aed3
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-11-17 12:16:23 +0400

    Update output.tf

--- terraform/modules/network/output.tf
+++ terraform/modules/network/output.tf
@@ -1,5 +1,5 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.Udacity-Urvesh-Test
+  value = azurerm_resource_group.Udacity-Urvesh-Test.name
 }
 output "subnet_id_test" {
   value = "${azurerm_subnet.test.id}"
