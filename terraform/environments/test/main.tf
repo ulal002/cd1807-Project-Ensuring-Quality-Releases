5e9b9005590b85e96e54c37b6f305c2f01c20cc1
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-11-17 14:43:49 +0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -39,3 +39,17 @@ module "nsg-test" {
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
 }
+module "appservice" {
+  source           = "../../modules/appservice"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "AppService"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
+module "publicip" {
+  source           = "../../modules/publicip"
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
+  resource_type    = "publicip"
+  resource_group   = "${module.resource_group.resource_group_name}"
+}
