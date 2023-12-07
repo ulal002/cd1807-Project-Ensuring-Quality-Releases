cc98c2506841556e22cf7ed5dab798bce5dce164
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-12-07 15:46:18 +0400

    Update add_remove_from_cart.py

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -1,3 +1,21 @@
+# #!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+
+# Start the browser and login with standard_user
+def login (user, password):
+    print ('Starting the browser...')
+    # --uncomment when running in Azure DevOps.
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
+    print ('Browser started successfully. Navigating to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+
+login('standard_user', 'secret_sauce')
+
 def add_cart(driver, n_items):
     for i in range(n_items):
         element = "a[id='item_" + str(i) + "_title_link']"  # Get the URL of the product
