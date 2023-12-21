ec42d929fd7fa6d536507e6cc3e8aae343381ad9
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-12-21 13:44:36 +0400

    Update add_remove_from_cart.py

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -2,9 +2,11 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import datetime
 
 def timestamp():
