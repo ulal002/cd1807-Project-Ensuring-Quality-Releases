3be75c33a33c90cb2f576332e5fc041bfa2f4472
ulal002 <139964364+ulal002@users.noreply.github.com>
2023-12-21 10:39:12 +0400

    Update add_remove_from_cart.py

--- automatedtesting/selenium/add_remove_from_cart.py
+++ automatedtesting/selenium/add_remove_from_cart.py
@@ -2,7 +2,6 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,27 +18,35 @@ def login(user, password):
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    service = Service(executable_path='/usr/local/bin/chromedriver')
-    driver = webdriver.Chrome(service=service, options=options)
-    #driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=options)
+    chromedriver_path = '/usr/local/bin/chromedriver'
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
     print(timestamp() + 'Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
-    from pyvirtualdisplay import Display
-    display = Display(visible=0, size=(1920, 1080))
-    display.start()
+    
+    # Login
+    driver.find_element(By.ID, 'user-name').send_keys(user)
+    driver.find_element(By.ID, 'password').send_keys(password)
+    driver.find_element(By.ID, 'login-button').click()
+    
     return driver
 
 def add_cart(driver, n_items):
     for i in range(n_items):
         element = "a[id='item_" + str(i) + "_title_link']"
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, element)))
-        item_link = driver.find_element(By.CSS_SELECTOR, element)
+        item_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, element)))
         item_link.click()
-        print(driver.page_source)
-        add_to_cart_button = driver.find_element(By.CSS_SELECTOR, "button.btn_primary.btn_inventory")
+        
+        add_to_cart_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn_primary.btn_inventory"))
+        )
         add_to_cart_button.click()
-        product = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_details_name']").text
+        
+        product = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.inventory_details_name"))
+        ).text
+        
         print(timestamp() + product + " added to the shopping cart.")
+        
         back_button = driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button")
         back_button.click()
     print(timestamp() + '{:d} items added to the shopping cart successfully.'.format(n_items))
@@ -47,16 +54,24 @@ def add_cart(driver, n_items):
 def remove_cart(driver, n_items):
     for i in range(n_items):
         element = "a[id='item_" + str(i) + "_title_link']"
-        item_link = driver.find_element(By.CSS_SELECTOR, element)
+        item_link = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, element)))
         item_link.click()
-        remove_from_cart_button = driver.find_element(By.CSS_SELECTOR, "button.btn_secondary.btn_inventory")
+        
+        remove_from_cart_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn_secondary.btn_inventory"))
+        )
         remove_from_cart_button.click()
-        product = driver.find_element(By.CSS_SELECTOR, "div[class='inventory_details_name']").text
+        
+        product = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.inventory_details_name"))
+        ).text
+        
         print(timestamp() + product + " removed from the shopping cart.")
+        
         back_button = driver.find_element(By.CSS_SELECTOR, "button.inventory_details_back_button")
         back_button.click()
     print(timestamp() + '{:d} items removed from the shopping cart successfully.'.format(n_items))
-    
+
 if __name__ == "__main__":
     N_ITEMS = 6
     TEST_USERNAME = 'standard_user'
